3ab30f31bceb755e11231a3cf2888a2231e0cdcc
nanxi <867362029@qq.com>
2025-06-26 16:48:47 +0800

    获取扫描的 node_modules 文件大小，不过很卡

--- scan-thread-pool/dist/index.js
+++ scan-thread-pool/dist/index.js
@@ -1,9 +1,35 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { Subject } from 'rxjs';
 import { ScanService } from './scan.js';
+import getFolderSize from 'get-folder-size';
 const stream = new Subject();
 const scanService = new ScanService();
 scanService.startScan('/Users/nx/Documents/study', stream);
-stream.subscribe((data) => {
-    console.log(data);
-});
+stream.subscribe((data) => __awaiter(void 0, void 0, void 0, function* () {
+    const size = yield getSize(data);
+    console.log('扫描结果：', data, size);
+}));
+function getSize(path) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const res = yield getFolderSize(path);
+            if (res && typeof res.size === 'number') {
+                return (res.size / 1024 / 1024) + 'M';
+            }
+            return '0M';
+        }
+        catch (error) {
+            console.error('获取文件夹大小失败:', error);
+            return '0M';
+        }
+    });
+}
 //# sourceMappingURL=index.js.map
--- scan-thread-pool/src/index.ts
+++ scan-thread-pool/src/index.ts
@@ -1,5 +1,6 @@
 import { Subject } from 'rxjs';
 import {ScanService} from './scan.js';
+import getFolderSize from 'get-folder-size';
 
 const stream = new Subject<string>();
 
@@ -7,6 +8,20 @@ const scanService = new ScanService();
 scanService.startScan('/Users/nx/Documents/study',stream);
 
 
-stream.subscribe((data) => {
-  console.log(data)
+stream.subscribe(async (data) => {
+  const size = await getSize(data);
+  console.log('扫描结果：',data,size)
 })
+
+async function getSize(path: string) {
+  try {
+    const res = await getFolderSize(path);
+    if (res && typeof res.size === 'number') {
+      return (res.size / 1024 / 1024) + 'M';
+    }
+    return '0M';
+  } catch (error) {
+    console.error('获取文件夹大小失败:', error);
+    return '0M';
+  }
+}
